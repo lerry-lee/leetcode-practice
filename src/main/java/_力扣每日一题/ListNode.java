a99fef909f4bc57272c819d3448d4fd72b522fa5
lca1070841799 <chunang_li@163.com>
2020-08-18 10:05:37 +0800

    update

---
+++ src/main/java/_力扣每日一题/ListNode.java
@@ -0,0 +1,25 @@
+package _力扣每日一题;
+
+/**
+ * @author lerrylee
+ * @version 1.0
+ * @create 2020/08/18 10:01
+ * @description TODO
+ */
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
