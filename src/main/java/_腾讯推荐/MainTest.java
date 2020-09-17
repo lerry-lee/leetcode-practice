ba18a39279779d9b2e0a7ad0be504b5b444c0d70
lerry-xps <chunang_li@163.com>
2020-09-17 15:32:29 +0800

    add 三数之和

--- src/main/java/_腾讯推荐/MainTest.java
+++ src/main/java/_腾讯推荐/MainTest.java
@@ -1,9 +1,6 @@
 package _腾讯推荐;
 
-import _腾讯推荐._数组与字符串._三数之和;
-import _腾讯推荐._数组与字符串._字符串转换整数;
-import _腾讯推荐._数组与字符串._寻找两个正序数组中的中位数;
-import _腾讯推荐._数组与字符串._最长公共前缀;
+import _腾讯推荐._数组与字符串.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -20,9 +17,15 @@ public class MainTest {
     public static void main(String[] args) {
         log.info("开始测试");
         long t1 = System.currentTimeMillis();
-        test_三数之和();
+        test_最接近的三数之和();
         long t2 = System.currentTimeMillis();
         log.info("测试结束，耗时:%dms", (t2 - t1));
+    }
+
+    public static void test_最接近的三数之和(){
+        int[] nums={-1,2,1,-4};
+        int[] nums1={0,2,1,-3};
+        System.out.println(new _最接近的三数之和().threeSumClosest(nums1,1));
     }
 
     public static void test_三数之和(){
---
+++ src/main/java/_腾讯推荐/_数组与字符串/_最接近的三数之和.java
@@ -0,0 +1,51 @@
+package _腾讯推荐._数组与字符串;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName : _最接近的三数之和
+ * @CreateTime : 2020/09/17 11
+ * @Author : lerry_li
+ * @Descrpition : 最接近的三数之和
+ * 给定一个包括n 个整数的数组nums和 一个目标值target。找出nums中的三个整数，使得它们的和与target最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+ * <p>
+ * <p>
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：nums = [-1,2,1,-4], target = 1
+ * 输出：2
+ * 解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+ */
+public class _最接近的三数之和 {
+    /**
+     * 解法1：转化成求两数之和，排序+双指针
+     */
+    public int threeSumClosest(int[] nums, int target) {
+        if (nums == null || nums.length < 3) return 0;
+        Arrays.sort(nums);
+        int min_dif = Math.abs(nums[0] + nums[1] + nums[2] - target);
+        int res_target = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; i++) {
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == target) {
+                    System.out.println(nums[i] + " " + nums[l] + " " + nums[r]);
+                    return target;
+                }
+                int temp_dif = Math.abs(sum - target);
+                if (temp_dif < min_dif) {
+                    min_dif = temp_dif;
+                    System.out.println(nums[i] + " " + nums[l] + " " + nums[r] + " :" + sum);
+                    res_target = sum;
+                    if(sum>target) r--;
+                    else l++;
+                } else if (sum > target) r--;
+                else l++;
+            }
+        }
+        return res_target;
+    }
+
+}
