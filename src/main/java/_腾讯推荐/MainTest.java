5d2a3abc09ee39fb2e4d67a5af2e81b9bbf78683
lerry-xps <chunang_li@163.com>
2020-09-17 10:19:32 +0800

    add 三数之和

--- src/main/java/_腾讯推荐/MainTest.java
+++ src/main/java/_腾讯推荐/MainTest.java
@@ -1,5 +1,6 @@
 package _腾讯推荐;
 
+import _腾讯推荐._数组与字符串._三数之和;
 import _腾讯推荐._数组与字符串._字符串转换整数;
 import _腾讯推荐._数组与字符串._寻找两个正序数组中的中位数;
 import _腾讯推荐._数组与字符串._最长公共前缀;
@@ -19,9 +20,15 @@ public class MainTest {
     public static void main(String[] args) {
         log.info("开始测试");
         long t1 = System.currentTimeMillis();
-        test_最长公共前缀();
+        test_三数之和();
         long t2 = System.currentTimeMillis();
         log.info("测试结束，耗时:%dms", (t2 - t1));
+    }
+
+    public static void test_三数之和(){
+        int[] nums={-1, 0, 1, 2, -1, -4};
+        int[] nums1={0,0,0};
+        System.out.println(new _三数之和().threeSum(nums));
     }
 
     public static void test_最长公共前缀(){
