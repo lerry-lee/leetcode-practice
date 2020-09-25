259d7ead5b2fceafca2bc6ff3968eb6d7204e978
lerry-xps <chunang_li@163.com>
2020-09-25 09:47:11 +0800

    add 二叉搜索树中的众数

--- src/main/java/_力扣每日一题/_20年9月/_501二叉搜索树中的众数.java
+++ src/main/java/_力扣每日一题/_20年9月/_501二叉搜索树中的众数.java
@@ -10,7 +10,35 @@ import java.util.Set;
 /**
  * Created by lerry_li on 2020/09/24
  */
+
+/**
+ *  二叉搜索树中的众数
+ * 给定一个有相同值的二叉搜索树（BST），找出 BST 中的所有众数（出现频率最高的元素）。
+ *
+ * 假定 BST 有如下定义：
+ *
+ * 结点左子树中所含结点的值小于等于当前结点的值
+ * 结点右子树中所含结点的值大于等于当前结点的值
+ * 左子树和右子树都是二叉搜索树
+ * 例如：
+ * 给定 BST [1,null,2,2],
+ *
+ *    1
+ *     \
+ *      2
+ *     /
+ *    2
+ * 返回[2].
+ *
+ * 提示：如果众数超过1个，不需考虑输出顺序
+ *
+ * 进阶：你可以不使用额外的空间吗？（假设由递归产生的隐式调用栈的开销不被计算在内）
+ */
 public class _501二叉搜索树中的众数 {
+    /**
+     * 解法1：中序遍历获得升序列表，然后遍历列表找到众数
+     * 解法2：利用二叉搜索树的性质，直接在中序遍历过程中记录并查找到众数
+     */
     List<Integer> mList = new ArrayList<>();
     int curent = 0;//表示当前节点的值
     int count = 0;//表示当前节点的数量
@@ -59,10 +87,5 @@ public class _501二叉搜索树中的众数 {
         //遍历右子树
         inOrderTraversal(node.right);
     }
-    public void dfs(List<Integer> list,TreeNode root){
-        if(root==null) return;
-        dfs(list,root.left);
-        list.add(root.val);
-        dfs(list,root.right);
-    }
+
 }
