854031821858d60279a02ef19953196380620fd8
lerry-xps <chunang_li@163.com>
2020-10-19 11:13:40 +0800

    add 排序链表

--- src/main/java/_腾讯推荐/_排序与搜索/_排序链表.java
+++ src/main/java/_腾讯推荐/_排序与搜索/_排序链表.java
@@ -5,25 +5,46 @@ import _数据结构.ListNode;
 /**
  * Created by lerry_li on 2020/10/18
  */
+
+/**
+ * 排序链表
+ * 在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
+ *
+ * 示例 1:
+ *
+ * 输入: 4->2->1->3
+ * 输出: 1->2->3->4
+ * 示例 2:
+ *
+ * 输入: -1->5->3->4->0
+ * 输出: -1->0->3->4->5
+ */
 public class _排序链表 {
+    /**
+     * 解法1：递归分治：时间O(nlogn) 空间O(1)（不算递归产生的额外空间的话）
+     */
     public ListNode sortList(ListNode head) {
-        if(head==null||head.next==null) return head;
-        ListNode node2=sortList(head.next);
-        return mergeSort(new ListNode(head.val),node2);
+        if (head == null || head.next == null) return head;
+        else {
+            ListNode fast=head.next,slow=head;
+            while(fast!=null&&fast.next!=null){
+                fast=fast.next.next;
+                slow=slow.next;
+            }
+            ListNode right = slow.next;
+            slow.next = null;
+            return mergeSort(sortList(head), sortList(right));
+        }
     }
-//    public ListNode sort(ListNode node){
-//        if(node.next==null) return node;
-//        ListNode node2=sort(node.next);
-//        return mergeSort(node,node2);
-//    }
-    public ListNode mergeSort(ListNode l1,ListNode l2){
-        if(l1==null) return l2;
-        if(l2==null) return l1;
-        if(l1.val<l2.val){
-            l1.next=mergeSort(l1.next,l2);
+
+    public ListNode mergeSort(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeSort(l1.next, l2);
             return l1;
-        }else{
-            l2.next=mergeSort(l1,l2.next);
+        } else {
+            l2.next = mergeSort(l1, l2.next);
             return l2;
         }
     }
