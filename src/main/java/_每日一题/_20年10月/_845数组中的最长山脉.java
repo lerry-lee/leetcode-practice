2dd51c843df99f7d9306246aec2beaf24ef50498
lerry-desktop <chunang_li@163.com>
2020-10-25 23:41:58 +0800

    add 数组中的最长山脉todo

--- src/main/java/_每日一题/_20年10月/_845数组中的最长山脉.java
+++ src/main/java/_每日一题/_20年10月/_845数组中的最长山脉.java
@@ -29,4 +29,35 @@ public class _845数组中的最长山脉 {
         }
         return res;
     }
+    /**
+     * 解法2：枚举山顶
+     */
+    public int longestMountain2(int[] A) {
+        if (A == null || A.length < 3) return 0;
+        int[] left=new int[A.length];
+        int[] right=new int[A.length];
+        left[0]=0;
+        right[A.length-1]=0;
+        for (int i = 1; i < A.length; i++) {
+            if(A[i]>A[i-1]){
+                left[i]=left[i-1]+1;
+            }else{
+                left[i]=0;
+            }
+        }
+        for (int i = A.length-2; i >=0 ; i--) {
+            if(A[i]>A[i+1]){
+                right[i]=right[i+1]+1;
+            }else{
+                right[i]=0;
+            }
+        }
+        int res=0;
+        for (int i = 1; i < A.length-1; i++) {
+            if(A[i]>A[i-1]&&A[i]>A[i+1]){
+                res=Math.max(res,left[i-1]+right[i+1]+1);
+            }
+        }
+        return res;
+    }
 }
