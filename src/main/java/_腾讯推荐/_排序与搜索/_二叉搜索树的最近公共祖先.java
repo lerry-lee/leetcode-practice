0b97dd16e3681a0a46f4bbf0c19c25da5b1ccfa4
lerry-xps <chunang_li@163.com>
2020-10-26 17:26:37 +0800

    add _二叉搜索树的最近公共祖先todo

---
+++ src/main/java/_腾讯推荐/_排序与搜索/_二叉搜索树的最近公共祖先.java
@@ -0,0 +1,12 @@
+package _腾讯推荐._排序与搜索;
+
+import _数据结构.TreeNode;
+
+/**
+ * Created by lerry_li on 2020/10/26
+ */
+public class _二叉搜索树的最近公共祖先 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+    }
+}
