0c2cc23d474cfc88c8cce9235e082291e66857b0
lerry-xps <chunang_li@163.com>
2020-10-30 10:30:43 +0800

    add 岛屿的周长

--- src/main/java/_每日一题/Main.java
+++ src/main/java/_每日一题/Main.java
@@ -29,204 +29,212 @@ public class Main {
     public static void main(String[] args) {
         log.info("开始测试");
         long t1 = System.currentTimeMillis();
-        test_1207();
+        test_463();
         long t2 = System.currentTimeMillis();
         log.info(String.format("测试结束，耗时:%dms", (t2 - t1)));
     }
 
-    public static void test_1207(){
-        int[] arr1={1,2,2,1,1,3};
-        int[] arr2={1,2};
-        int[] arr3={-3,0,1,-3,1,1,1,-3,10,0};
+    public static void test_463() {
+        int[][] grid = {{0, 1, 0, 0},
+                {1, 1, 1, 0},
+                {0, 1, 0, 0},
+                {1, 1, 0, 0}};
+        System.out.println(new _463岛屿的周长().islandPerimeter2(grid));
+    }
+
+    public static void test_1207() {
+        int[] arr1 = {1, 2, 2, 1, 1, 3};
+        int[] arr2 = {1, 2};
+        int[] arr3 = {-3, 0, 1, -3, 1, 1, 1, -3, 10, 0};
         System.out.println(new _1207独一无二的出现次数().uniqueOccurrences(arr1));
         System.out.println(new _1207独一无二的出现次数().uniqueOccurrences(arr2));
         System.out.println(new _1207独一无二的出现次数().uniqueOccurrences(arr3));
     }
 
-    public static void test_845(){
-        int[] A={4,2,9,8,0};
-        int[] A1={2,1,4,7,3,2,5};
+    public static void test_845() {
+        int[] A = {4, 2, 9, 8, 0};
+        int[] A1 = {2, 1, 4, 7, 3, 2, 5};
         System.out.println(new _845数组中的最长山脉().longestMountain(A1));
     }
 
-    public static void test_1024(){
-        int[][] A={{0,2},{4,8}};
-        System.out.println(new _1024视频拼接().videoStitching(A,5));
+    public static void test_1024() {
+        int[][] A = {{0, 2}, {4, 8}};
+        System.out.println(new _1024视频拼接().videoStitching(A, 5));
     }
 
-    public static void test_243(){
-        ListNode head=new ListNode(1);
-        head.next=new ListNode(0);
-        head.next.next=new ListNode(1);
+    public static void test_243() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(0);
+        head.next.next = new ListNode(1);
         System.out.println(new _243回文链表().isPalindrome(head));
     }
 
-    public static void test_763(){
-        String s="ababcbacadefegdehijhklij";
+    public static void test_763() {
+        String s = "ababcbacadefegdehijhklij";
         System.out.println(new _763划分字母区间().partitionLabels(s));
     }
 
-    public static void test_925(){
-        while(true){
-            String name=getString();
-            String typed=getString();
-            System.out.println(new _925长按键入().isLongPressedName(name,typed));
+    public static void test_925() {
+        while (true) {
+            String name = getString();
+            String typed = getString();
+            System.out.println(new _925长按键入().isLongPressedName(name, typed));
         }
     }
 
-    public static void test_844(){
-        while(true){
-            String S=getString(),T=getString();
-            System.out.println(new _844比较含退格的字符串().backspaceCompare(S,T));
+    public static void test_844() {
+        while (true) {
+            String S = getString(), T = getString();
+            System.out.println(new _844比较含退格的字符串().backspaceCompare(S, T));
         }
 
     }
 
-    public static void test_977(){
-        int[] A={-7,-3,2,3,11};
+    public static void test_977() {
+        int[] A = {-7, -3, 2, 3, 11};
         display(new _977有序数组的平方().sortedSquares(A));
     }
 
-    public static void test_52(){
+    public static void test_52() {
         System.out.println(new _52N皇后2().totalNQueens(4));
     }
 
-    public static void test_1002(){
+    public static void test_1002() {
 //        String[] A={"cool","lock","cook"};
-        String[] A={"bella","label","roller"};
-        List<String> r0=new _1002查找常用字符().commonChars(A);
+        String[] A = {"bella", "label", "roller"};
+        List<String> r0 = new _1002查找常用字符().commonChars(A);
         System.out.println(r0.size());
-        String[] r1={"a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"};
+        String[] r1 = {"a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a"};
         System.out.println(r1.length);
         System.out.println(A.length);
     }
 
-    public static void test_416(){
-        int[] nums={14,9,8,4,3,2};
-        System.out.println(""+(14+9+8+4+3+2));
+    public static void test_416() {
+        int[] nums = {14, 9, 8, 4, 3, 2};
+        System.out.println("" + (14 + 9 + 8 + 4 + 3 + 2));
         System.out.println(new _416分割等和子集().canPartition(nums));
     }
 
-    public static void test_75(){
+    public static void test_75() {
 //        int[] nums={2,0,2,1,1,0};
-        int[] nums={2,0,1};
+        int[] nums = {2, 0, 1};
         display(nums);
         new _75颜色分类().sortColors_(nums);
         display(nums);
     }
 
-    public static void test_18(){
-        int[] nums={-2,-1,-1,1,1,2,2};
-        System.out.println(new _18四数之和().fourSum_2pointer(nums,0));
+    public static void test_18() {
+        int[] nums = {-2, -1, -1, 1, 1, 2, 2};
+        System.out.println(new _18四数之和().fourSum_2pointer(nums, 0));
     }
 
-    public static void test_19(){
-        String s="rrryyyrryyyrr";
+    public static void test_19() {
+        String s = "rrryyyrryyyrr";
         System.out.println(s);
         new _19秋叶收藏集().minimumOperations(s);
     }
 
-    public static void test_235(){
-        TreeNode root=new TreeNode(6);
-        insertLeftAndRightChildToTreeNode(root,2,8);
-        insertLeftAndRightChildToTreeNode(root.left,0,4);
-        insertLeftAndRightChildToTreeNode(root.left.right,3,5);
-        insertLeftAndRightChildToTreeNode(root.right,7,9);
-        System.out.println(new _235二叉搜索树的最近公共祖先().lowestCommonAncestor(root,root.left.right,root.left.left).val);
+    public static void test_235() {
+        TreeNode root = new TreeNode(6);
+        insertLeftAndRightChildToTreeNode(root, 2, 8);
+        insertLeftAndRightChildToTreeNode(root.left, 0, 4);
+        insertLeftAndRightChildToTreeNode(root.left.right, 3, 5);
+        insertLeftAndRightChildToTreeNode(root.right, 7, 9);
+        System.out.println(new _235二叉搜索树的最近公共祖先().lowestCommonAncestor(root, root.left.right, root.left.left).val);
     }
 
-    public static void test_113(){
-        TreeNode root=new TreeNode(5);
-        insertLeftAndRightChildToTreeNode(root,4,8);
-        insertLeftAndRightChildToTreeNode(root.left,11,2333);
-        insertLeftAndRightChildToTreeNode(root.left.left,7,2);
-        insertLeftAndRightChildToTreeNode(root.right,13,4);
-        insertLeftAndRightChildToTreeNode(root.right.right,5,1);
-        System.out.println(new _113路径总和2().pathSum(root,22));
+    public static void test_113() {
+        TreeNode root = new TreeNode(5);
+        insertLeftAndRightChildToTreeNode(root, 4, 8);
+        insertLeftAndRightChildToTreeNode(root.left, 11, 2333);
+        insertLeftAndRightChildToTreeNode(root.left.left, 7, 2);
+        insertLeftAndRightChildToTreeNode(root.right, 13, 4);
+        insertLeftAndRightChildToTreeNode(root.right.right, 5, 1);
+        System.out.println(new _113路径总和2().pathSum(root, 22));
     }
 
-    public static void insertLeftAndRightChildToTreeNode(TreeNode root,int leftVal,int rightVal){
-        if(leftVal!=2333) root.left=new TreeNode(leftVal);
-        if(rightVal!=2333) root.right=new TreeNode(rightVal);
+    public static void insertLeftAndRightChildToTreeNode(TreeNode root, int leftVal, int rightVal) {
+        if (leftVal != 2333) root.left = new TreeNode(leftVal);
+        if (rightVal != 2333) root.right = new TreeNode(rightVal);
     }
 
-    public static void tet_106(){
-        int[] inorder={2,1};
-        int[] postorder={2,1};
-        display(new _106从中序与后续遍历序列构造二叉树().buildTree(inorder,postorder));
+    public static void tet_106() {
+        int[] inorder = {2, 1};
+        int[] postorder = {2, 1};
+        display(new _106从中序与后续遍历序列构造二叉树().buildTree(inorder, postorder));
     }
 
-    public static void test_501(){
-        TreeNode root=new TreeNode(2147483647);
-        TreeNode root1=new TreeNode(1);
-        root1.right=new TreeNode(2);
-        root1.right.left=new TreeNode(2);
+    public static void test_501() {
+        TreeNode root = new TreeNode(2147483647);
+        TreeNode root1 = new TreeNode(1);
+        root1.right = new TreeNode(2);
+        root1.right.left = new TreeNode(2);
 
-        int[] a=new _501二叉搜索树中的众数().findMode(root1);
+        int[] a = new _501二叉搜索树中的众数().findMode(root1);
         display(a);
     }
 
-    public static void test_968(){
-        TreeNode root=new TreeNode(0);
-        root.left=new TreeNode(1);
-        root.left.left=new TreeNode(2);
-        root.left.right=new TreeNode(2);
+    public static void test_968() {
+        TreeNode root = new TreeNode(0);
+        root.left = new TreeNode(1);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(2);
         System.out.println(new _968监控二叉树().minCameraCover(root));
     }
 
-    public static void test_538(){
-        TreeNode root=new TreeNode(5);
-        root.left=new TreeNode(2);
-        root.right=new TreeNode(13);
+    public static void test_538() {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(13);
 //        root.left.left=new TreeNode(-4);
 //        root.left.right=new TreeNode(1);
         display(new _538把二叉搜索树转换为累加树().convertBST_b(root));
 
     }
 
-    public static ListNode getListNode(){
-        Scanner sc=new Scanner(System.in);
-        ListNode listNode=new ListNode();
-        ListNode head=listNode;
-        int n=sc.nextInt();
+    public static ListNode getListNode() {
+        Scanner sc = new Scanner(System.in);
+        ListNode listNode = new ListNode();
+        ListNode head = listNode;
+        int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
-            listNode=new ListNode(sc.nextInt());
-            listNode=listNode.next;
+            listNode = new ListNode(sc.nextInt());
+            listNode = listNode.next;
         }
         return head;
     }
 
-    public static String getString(){
-        Scanner sc=new Scanner(System.in);
+    public static String getString() {
+        Scanner sc = new Scanner(System.in);
         return sc.next();
     }
 
-    public static int[] getNums(){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[] nums=new int[n];
+    public static int[] getNums() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
         for (int i = 0; i < n; i++) {
-            nums[i]=sc.nextInt();
+            nums[i] = sc.nextInt();
         }
         sc.close();
         return nums;
     }
 
-    public static void test_78(){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[] nums=new int[n];
+    public static void test_78() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
         for (int i = 0; i < n; i++) {
-            nums[i]=sc.nextInt();
+            nums[i] = sc.nextInt();
         }
         System.out.println(new _78子集().subsets(nums));
         sc.close();
     }
 
-    public static void test_47(){
-        int[] nums={1,1,2,};
-        int[] nums1={1,2,3};
-        int[] nums2={2,2,1,1};
+    public static void test_47() {
+        int[] nums = {1, 1, 2,};
+        int[] nums1 = {1, 2, 3};
+        int[] nums2 = {2, 2, 1, 1};
         System.out.println(new _47全排列2().permuteUnique(nums));
         System.out.println(new _47全排列2().permuteUnique_swap(nums));
         System.out.println();
@@ -237,27 +245,27 @@ public class Main {
         System.out.println(new _47全排列2().permuteUnique_swap(nums2));
     }
 
-    public static void test_226(){
-        TreeNode root=new TreeNode(4);
-        root.left=new TreeNode(2);
-        root.left.left=new TreeNode(1);
-        root.left.right=new TreeNode(3);
-        root.right=new TreeNode(7);
-        root.right.left=new TreeNode(6);
-        root.right.right=new TreeNode(9);
+    public static void test_226() {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right = new TreeNode(7);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
         new _226翻转二叉树().invertTree(root);
         display(root);
     }
 
-    public static void display(TreeNode root){
-        if(root==null) return;
+    public static void display(TreeNode root) {
+        if (root == null) return;
         System.out.println(root.val);
         display(root.left);
         display(root.right);
     }
 
-    public static void test_最长回文子串(){
-        String s="babad";
+    public static void test_最长回文子串() {
+        String s = "babad";
         System.out.println(new _最长回文子串().longestPalindrome_dp(s));
     }
 
@@ -267,11 +275,11 @@ public class Main {
                 {'S', 'F', 'C', 'S'},
                 {'A', 'D', 'E', 'E'}
         };
-        char[][] board1={
-                {'a','b'}
+        char[][] board1 = {
+                {'a', 'b'}
         };
-        String word="ab";
-        System.out.println(new _79单词搜索().exist(board1,word));
+        String word = "ab";
+        System.out.println(new _79单词搜索().exist(board1, word));
 
     }
 
