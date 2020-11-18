d57179b0820127cc0c10c2d58d96f4dbf6fdaec8
lerry-xps <chunang_li@163.com>
2020-11-18 15:29:58 +0800

    add 矩阵中的路径

--- src/main/java/SwordFingerOffer/_12矩阵中的路径.java
+++ src/main/java/SwordFingerOffer/_12矩阵中的路径.java
@@ -16,47 +16,39 @@ public class _12矩阵中的路径 {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 if (board[i][j] == word.charAt(0)) {
-                    dfs(board, new boolean[board.length][board[0].length], i, j, word, 0);
-                    if (flag) {
+                    if (dfs(board, new boolean[board.length][board[0].length], i, j, word, 0)) {
                         return true;
                     }
                 }
             }
         }
-        return flag;
+        return false;
     }
 
-    boolean flag;
-
-    public void dfs(char[][] board, boolean[][] visited, int row, int col, String word, int wordIdx) {
-        if (wordIdx == word.length()) {
-            flag = true;
-            return;
-        }
+    public boolean dfs(char[][] board, boolean[][] visited, int row, int col, String word, int wordIdx) {
         char c = word.charAt(wordIdx);
         if (row < 0 || col < 0 || row == board.length || col == board[row].length || visited[row][col] || board[row][col] != c) {
-            return;
+            return false;
         }
+        if (wordIdx == word.length() - 1) {
+            return true;
+        }
+
         visited[row][col] = true;
-        dfs(board, visited, row - 1, col, word, wordIdx + 1);
-        if (!flag) {
-            dfs(board, visited, row + 1, col, word, wordIdx + 1);
-        }
-        if (!flag) {
-            dfs(board, visited, row, col - 1, word, wordIdx + 1);
-        }
-        if (!flag) {
-            dfs(board, visited, row, col + 1, word, wordIdx + 1);
-        }
+        boolean res = dfs(board, visited, row - 1, col, word, wordIdx + 1) ||
+                dfs(board, visited, row + 1, col, word, wordIdx + 1) ||
+                dfs(board, visited, row, col - 1, word, wordIdx + 1) ||
+                dfs(board, visited, row, col + 1, word, wordIdx + 1);
         visited[row][col] = false;
+        return res;
     }
 
     public static void main(String[] args) {
         _12矩阵中的路径 instance = new _12矩阵中的路径();
-//        char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
-        char[][] board = {{'a', 'b'}, {'c', 'd'}};
-        String word = "abcd";
-//        String word = "ABCCED";
+        char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+//        char[][] board = {{'a', 'b'}, {'c', 'd'}};
+//        String word = "abcd";
+        String word = "ABCCED";
         System.out.println(instance.exist(board, word));
     }
 }
