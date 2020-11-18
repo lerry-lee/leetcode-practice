f4217b44dea23f9d747e77501dbd719b8133855e
lerry-xps <chunang_li@163.com>
2020-11-18 11:12:47 +0800

    add 矩阵中的路径

---
+++ src/main/java/SwordFingerOffer/_12矩阵中的路径.java
@@ -0,0 +1,62 @@
+package SwordFingerOffer;
+
+/**
+ * @ClassName: _12矩阵中的路径
+ * @Signature: Created by lerry_li on 2020/11/18
+ * @Description:
+ */
+public class _12矩阵中的路径 {
+    /**
+     * 解法1：回溯
+     */
+    public boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0 || word == null || word.length() == 0) {
+            return false;
+        }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    dfs(board, new boolean[board.length][board[0].length], i, j, word, 0);
+                    if (flag) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return flag;
+    }
+
+    boolean flag;
+
+    public void dfs(char[][] board, boolean[][] visited, int row, int col, String word, int wordIdx) {
+        if (wordIdx == word.length()) {
+            flag = true;
+            return;
+        }
+        char c = word.charAt(wordIdx);
+        if (row < 0 || col < 0 || row == board.length || col == board[row].length || visited[row][col] || board[row][col] != c) {
+            return;
+        }
+        visited[row][col] = true;
+        dfs(board, visited, row - 1, col, word, wordIdx + 1);
+        if (!flag) {
+            dfs(board, visited, row + 1, col, word, wordIdx + 1);
+        }
+        if (!flag) {
+            dfs(board, visited, row, col - 1, word, wordIdx + 1);
+        }
+        if (!flag) {
+            dfs(board, visited, row, col + 1, word, wordIdx + 1);
+        }
+        visited[row][col] = false;
+    }
+
+    public static void main(String[] args) {
+        _12矩阵中的路径 instance = new _12矩阵中的路径();
+//        char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+        char[][] board = {{'a', 'b'}, {'c', 'd'}};
+        String word = "abcd";
+//        String word = "ABCCED";
+        System.out.println(instance.exist(board, word));
+    }
+}
