1e00f0bc364f32dda68b4cd840834ee586a9c726
lerry-xps <chunang_li@163.com>
2021-01-06 10:28:40 +0800

    add _399除法求值 todo

---
+++ src/main/java/_每日一题/_2021年/_21年1月/_399除法求值.java
@@ -0,0 +1,17 @@
+package _每日一题._2021年._21年1月;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ClassName: _399除法求值
+ * @Author: lerry_li
+ * @CreateDate: 2021/01/06
+ * @Description
+ */
+public class _399除法求值 {
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+    }
+}
