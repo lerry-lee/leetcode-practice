64322555cebcd6f773e7ee736df8fc11f7c33944
lerry-desktop <chunang_li@163.com>
2021-02-25 10:15:28 +0800

    add 转置矩阵

--- src/main/java/_每日一题/_2021年/_21年2月/_867转置矩阵.java
+++ src/main/java/_每日一题/_2021年/_21年2月/_867转置矩阵.java
@@ -15,6 +15,18 @@ public class _867转置矩阵 {
             return matrix;
         }
         int m = matrix.length, n = matrix[0].length;
+
+        if (m == n) {
+            for (int i = 0; i < m; i++) {
+                for (int j = i+1; j < n; j++) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+            return matrix;
+        }
+
         int[][] res = new int[n][m];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
