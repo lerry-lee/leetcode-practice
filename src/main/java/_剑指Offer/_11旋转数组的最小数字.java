d080341eaecaf549cf04e9939bde4e6f4159106d
lerry-desktop <chunang_li@163.com>
2021-04-02 21:15:09 +0800

    add 剑指offer 旋转数组的最小数字

---
+++ src/main/java/_剑指Offer/_11旋转数组的最小数字.java
@@ -0,0 +1,40 @@
+package _剑指Offer;
+
+/**
+ * @ClassName: _11旋转数组的最小数字
+ * @Author: lerry_li
+ * @CreateTime: 2021/04/02
+ * @Description
+ */
+public class _11旋转数组的最小数字 {
+    /**
+     * 解法1：二分 时间O(logN) 空间O(1)
+     */
+    public int minArray(int[] numbers) {
+        int left = 0, right = numbers.length - 1;
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            if (numbers[mid] > numbers[right]) {
+                // [3, 4, 5, 1, 2]，mid 以及 mid 的左边一定不是最小数字
+                // 下一轮搜索区间是 [mid + 1, right]
+                left = mid + 1;
+            } else if (numbers[mid] == numbers[right]) {
+                // 只能把 right 排除掉，下一轮搜索区间是 [left, right - 1]
+                right = right - 1;
+            } else {
+                // 此时 numbers[mid] < numbers[right]
+                // mid 的右边一定不是最小数字，mid 有可能是，下一轮搜索区间是 [left, mid]
+                right = mid;
+            }
+        }
+        return numbers[left];
+    }
+
+    public static void main(String[] args) {
+        _11旋转数组的最小数字 instance=new _11旋转数组的最小数字();
+        System.out.println(instance.minArray(new int[]{3,4,5,1,2}));
+        System.out.println(instance.minArray(new int[]{4,5,1,2,3}));
+        System.out.println(instance.minArray(new int[]{5,1,2,3,4}));
+
+    }
+}
