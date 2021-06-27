40468441633298a00af1310c14bd35e8254f9472
lerry-xps <chunang_li@163.com>
2021-06-27 13:27:19 +0800

    add _483最小好进制

--- src/main/java/_每日一题/_2021年/_21年6月/_483最小好进制.java
+++ src/main/java/_每日一题/_2021年/_21年6月/_483最小好进制.java
@@ -7,7 +7,23 @@ package _每日一题._2021年._21年6月;
  * @Description todo
  */
 public class _483最小好进制 {
+    /**
+     * 解法1：数学
+     */
     public String smallestGoodBase(String n) {
-        return null;
+        long nVal = Long.parseLong(n);
+        int mMax = (int) Math.floor(Math.log(nVal) / Math.log(2));
+        for (int m = mMax; m > 1; m--) {
+            int k = (int) Math.pow(nVal, 1.0 / m);
+            long mul = 1, sum = 1;
+            for (int i = 0; i < m; i++) {
+                mul *= k;
+                sum += mul;
+            }
+            if (sum == nVal) {
+                return Integer.toString(k);
+            }
+        }
+        return Long.toString(nVal - 1);
     }
 }
