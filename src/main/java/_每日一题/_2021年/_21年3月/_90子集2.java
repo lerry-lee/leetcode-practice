5bf3b4426a68f5e7ef08704fd85f0c9c21c31d2a
lerry-xps <chunang_li@163.com>
2021-07-24 16:32:04 +0800

    update

--- src/main/java/_每日一题/_2021年/_21年3月/_90子集2.java
+++ src/main/java/_每日一题/_2021年/_21年3月/_90子集2.java
@@ -17,16 +17,25 @@ public class _90子集2 {
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         res = new ArrayList<>();
+        //特判
         if (nums == null || nums.length == 0) {
             return res;
         }
+        //排序
         Arrays.sort(nums);
-
+        //递归回溯
         dfs(nums, new ArrayList<>(), 0);
         return res;
     }
 
+    /**
+     * 递归回溯，回溯的所有解构成一棵树
+     * @param nums 整数数组
+     * @param path 记录当前路径
+     * @param t 数组下标
+     */
     public void dfs(int[] nums, List<Integer> path, int t) {
+        //添加当前路径(一个解)到res
         res.add(new ArrayList<>(path));
         for (int i = t; i < nums.length; i++) {
             if (i > t && nums[i] == nums[i - 1]) {
