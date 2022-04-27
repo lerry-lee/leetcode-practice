dbda1bccf7b3cb0867f25ca64365bd15d5e2e9d6
lerry <chunang_li@163.com>
2022-04-27 17:26:20 +0800

    _48_旋转图像

---
+++ src/main/java/_每日一题/_2022年/_48_旋转图像.java
@@ -0,0 +1,38 @@
+package _每日一题._2022年;
+
+/**
+ * @Author: lerry_li
+ * @CreateDate: 2022/04/27
+ * @Description
+ */
+public class _48_旋转图像 {
+
+    public static void main(String[] args) {
+        _48_旋转图像 instance=new _48_旋转图像();
+        instance.new Solution().rotate(new int[][]{{1,2,3},{4,5,6},{7,8,9}});
+    }
+
+    /**
+     * 解法1：原地置换 时间O(N^2) 空间O(1)
+     * 思路：
+     *      按圈遍历，每次遍历一行，圈逐渐缩小
+     */
+    class Solution {
+        public void rotate(int[][] matrix) {
+            if(matrix==null||matrix.length==0) return;
+            int n=matrix.length;
+            //0行~n/2-1行即可
+            for(int i=0;i<n/2;i++){
+                //圈尾部最后一列是n-1-i，j不用遍历到最后一个元素，因为在置换时，最后一个元素被置换了
+                for(int j=i;j<n-1-i;j++){
+                    //记录第一个被置换的值，然后用矩阵顺时针90后的该位置的元素替换它
+                    int temp=matrix[i][j];
+                    matrix[i][j]=matrix[n-1-j][i];
+                    matrix[n-1-j][i]=matrix[n-1-i][n-1-j];
+                    matrix[n-1-i][n-1-j]=matrix[j][n-1-i];
+                    matrix[j][n-1-i]=temp;
+                }
+            }
+        }
+    }
+}
